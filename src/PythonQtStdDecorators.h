44a61a93c821647ca7cd759769ce5c1e88a57e6c
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2021-01-29 10:16:06 +0100

    reimplement (s)rand

--- src/PythonQtStdDecorators.h
+++ src/PythonQtStdDecorators.h
@@ -52,6 +52,7 @@
 #include <QColor>
 #include <QDateTime>
 #include <QDate>
+#include <QRandomGenerator>
 #include <QRegExp>
 #include <QTime>
 #include <QTimer>
@@ -63,6 +64,7 @@
 class PYTHONQT_EXPORT PythonQtStdDecorators : public QObject
 {
   Q_OBJECT
+  QRandomGenerator rng;
 
 public Q_SLOTS:
   bool connect(QObject* sender, const QByteArray& signal, PyObject* callable);
@@ -105,8 +107,8 @@ public Q_SLOTS:
   int static_Qt_qRound(double a) { return qRound(a); }
   qint64 static_Qt_qRound64(double a) { return qRound64(a); }
   const char* static_Qt_qVersion() { return qVersion(); }
-//  int static_Qt_qrand() { return qrand(); }
-//  void static_Qt_qsrand(uint a) { qsrand(a); }
+  int static_Qt_qrand() { return rng(); }
+  void static_Qt_qsrand(uint a) { rng.seed(a); }
 
   QString tr(QObject* obj, const QString& text, const QString& ambig = QString(), int n = -1);
 
