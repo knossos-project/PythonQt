be5c93dcd810c49ed1df796ef3989f9e0f721f99
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2022-10-25 05:53:12 +0200

    3.10

--- src/python data exports.h
+++ src/python data exports.h
@@ -1,5 +1,6 @@
 #pragma once
 
-#include "python27 data exports.h"
+//#include "python27 data exports.h"
 //#include "python38 data exports.h"
 //#include "python39 data exports.h"
+#include "python310 data exports.h"
