b80af99517b0158a4ba53b40b92cf1b0c5c10d2f
Norbert Pfeiler <norbert.pfeiler+git@gmail.com>
2022-10-25 17:01:35 +0200

    unify

--- src/python data exports.h
+++ src/python data exports.h
@@ -1,6 +1,28 @@
 #pragma once
 
-//#include "python27 data exports.h"
-//#include "python38 data exports.h"
-//#include "python39 data exports.h"
-#include "python310 data exports.h"
+#include <libloaderapi.h>
+
+#include <boost/preprocessor/stringize.hpp>
+
+#include <QDebug>
+
+//#define optpy python27
+#define optpy python310
+
+static auto pythonDll = [](){
+    static auto pythonDll = GetModuleHandle(BOOST_PP_STRINGIZE(optpy.dll));
+    if (pythonDll == nullptr) {
+        throw 1;
+    }
+    //qDebug() << "pythonDll" << pythonDll;
+    return pythonDll;
+};
+static auto GetProcAddress2 = [](auto name){
+    //qDebug() << pythonDll() << name << reinterpret_cast<std::size_t>(GetProcAddress(pythonDll(), name));
+    return GetProcAddress(pythonDll(), name);
+};
+
+#define LoadPythonSymbol(name) (*reinterpret_cast<decltype(name)*>(GetProcAddress2(#name)))
+
+
+#include BOOST_PP_STRINGIZE(optpy data exports.h)
